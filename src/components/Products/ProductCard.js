16319e10d8b97bd839ab48c2419c8b2bdb4191ac
Salman Zafar <maanizfar@gmail.com>
2020-07-10 11:54:34 +0500

    update product card design

--- src/components/Products/ProductCard.js
+++ src/components/Products/ProductCard.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import clsx from "clsx";
-// import GlobalContext from "../state/GlobalContext";
+import GlobalContext from "../../state/GlobalContext";
 import { useNavigate } from "react-router-dom";
+import IconButton from "@material-ui/core/IconButton";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import { Hidden } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,10 +24,10 @@ const useStyles = makeStyles((theme) => ({
     height: 300,
 
     [theme.breakpoints.down("md")]: {
-      height: 220,
+      height: 200,
     },
     [theme.breakpoints.down("xs")]: {
-      height: 180,
+      height: 120,
     },
   },
 
@@ -38,13 +41,6 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.only("xs")]: {
       flexDirection: "column",
     },
-  },
-
-  addToCardBtn: {
-    position: "absolute",
-    fontSize: "2rem",
-    right: 2,
-    top: 2,
   },
 
   h3ResponsiveText: {
@@ -62,61 +58,57 @@ const useStyles = makeStyles((theme) => ({
     },
   },
 
-  priceText: {
-    paddingLeft: theme.spacing(2),
-    position: "absolute",
-    top: 14,
-    left: 2,
+  row: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "flex-start",
+    width: "100%",
   },
 }));
 
 const ProductCard = ({ product }) => {
   const classes = useStyles();
-  // const { addToCart } = useContext(GlobalContext);
+  const { addToCart } = useContext(GlobalContext);
   const navigate = useNavigate();
 
-  // function handleAddToCart() {
-  //   addToCart(product);
-  // }
+  function handleAddToCart(e) {
+    e.stopPropagation();
+    addToCart(product);
+  }
 
-  function handleClick() {
+  function handleClick(e) {
+    console.log(e.target.value);
     navigate(`/products/${product.slug}`);
   }
 
   return (
     <Card className={classes.root} onClick={handleClick}>
-      <Typography
-        variant="h6"
-        component="h3"
-        className={clsx(classes.h3ResponsiveText, classes.priceText)}
-        align="right"
-      >
-        ${product.price}
-      </Typography>
-      {/* {product.items_left > 0 && (
-        <IconButton
-          color="inherit"
-          className={classes.addToCardBtn}
-          onClick={handleAddToCart}
-        >
-          <AddShoppingCartIcon />
-        </IconButton>
-      )} */}
       <CardMedia
         className={classes.media}
         image={product.imageURL}
         title={product.name}
       />
       <CardContent className={classes.cardContent}>
-        <div>
-          <Typography
-            variant="h6"
-            component="h3"
-            gutterBottom
-            className={classes.h3ResponsiveText}
-          >
-            {product.name}
-          </Typography>
+        <div style={{ width: "100%" }}>
+          <div className={classes.row}>
+            <Typography
+              variant="h6"
+              component="h3"
+              className={classes.h3ResponsiveText}
+            >
+              {product.name}
+            </Typography>
+            <Hidden xsDown>
+              <Typography
+                variant="h6"
+                component="h3"
+                className={clsx(classes.h3ResponsiveText)}
+                align="right"
+              >
+                ${product.price}
+              </Typography>
+            </Hidden>
+          </div>
           <Typography
             variant="button"
             color="textSecondary"
@@ -126,16 +118,30 @@ const ProductCard = ({ product }) => {
             {product.brand}
           </Typography>
           {product.items_left > 0 ? (
-            <>
-              <Typography
-                variant="button"
-                color="textSecondary"
-                component="p"
-                className={classes.btnResponsiveText}
-              >
-                Items left: <strong>{product.items_left}</strong>
-              </Typography>
-            </>
+            <div className={classes.row} style={{ alignItems: "center" }}>
+              <Hidden xsDown>
+                <Typography
+                  variant="button"
+                  color="textSecondary"
+                  component="p"
+                  className={classes.btnResponsiveText}
+                >
+                  Items left: <strong>{product.items_left}</strong>
+                </Typography>
+              </Hidden>
+              <Hidden smUp>
+                <Typography
+                  variant="h4"
+                  className={clsx(classes.h3ResponsiveText)}
+                  align="right"
+                >
+                  ${product.price}
+                </Typography>
+              </Hidden>
+              <IconButton color="inherit" onClick={handleAddToCart}>
+                <AddShoppingCartIcon />
+              </IconButton>
+            </div>
           ) : (
             <Typography variant="button" color="error" component="p">
               Out of Stock
